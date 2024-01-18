ffda59227f78ebeb38abf0fcddd5c794d73ca180
Maria Shchurova <mary.shchurova@gmail.com>
2024-01-18 01:16:12 +0100

    3rd person c++ project

---
+++ Source/UE5_SoulsLike/UE5_SoulsLike.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_SoulsLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_SoulsLike, "UE5_SoulsLike" );
+ 
