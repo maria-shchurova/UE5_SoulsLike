dc71fa580d2e95eb866e92b46fb821f9cf1ff371
Maria Shchurova <mary.shchurova@gmail.com>
2024-01-22 18:24:14 +0100

    code cleanup and faster blocking input registering

--- Source/UE5_SoulsLike/UE5_SoulsLikeCharacter.h
+++ Source/UE5_SoulsLike/UE5_SoulsLikeCharacter.h
@@ -15,14 +15,6 @@ class AUE5_SoulsLikeCharacter : public ACharacter, public IAbilitySystemInterfac
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Abilities, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-	*/
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UAbilitySystemComponent* AbilitySystemComponent;
 
@@ -31,48 +23,7 @@ class AUE5_SoulsLikeCharacter : public ACharacter, public IAbilitySystemInterfac
 
 public:
 	AUE5_SoulsLikeCharacter();
-
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Input)
-	float TurnRateGamepad;
-
-protected:
-
-	/** Called for forwards/backward input 
-	void MoveForward(float Value);
-
-	/** Called for side to side input 
-	void MoveRight(float Value);
-
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 
-	void LookUpAtRate(float Rate);
-
-	/** Handler for when a touch input begins. 
-	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
-
-	/** Handler for when a touch input stops. 
-	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
-	
-protected:
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	// End of APawn interface
-	*/
 public:
-	/** Returns CameraBoom subobject **/
-	//FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	//FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
 	// Inherited via IAbilitySystemInterface
 	UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
